6bed23627f9d54becd1000e718cd0682258454c5
spardano <sakti.pardano29@gmail.com>
2021-07-02 04:55:44 +0700

    commit pertama

---
+++ application/libraries/User_login.php
@@ -0,0 +1,54 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User_login
+{
+    protected $ci;
+
+    public function __construct()
+    {
+        $this->ci = &get_instance();
+        $this->ci->load->model('m_auth');
+    }
+
+    //cek login
+    public function login($username, $password)
+    {
+        $cek = $this->ci->m_auth->login_user($username, $password);
+        if ($cek) {
+            $nama_user = $cek->nama_user;
+            $username = $cek->username;
+            $level_user = $cek->level_user;
+
+            //buat seesion
+            $this->ci->session->set_userdata('username', $username);
+            $this->ci->session->set_userdata('nama_user', $nama_user);
+            $this->ci->session->set_userdata('level_user', $level_user);
+            redirect('admin');
+        } else {
+            //login salah
+            $this->ci->session->set_flashdata('error', 'Username Atau Password Salah');
+            redirect('auth/login_user');
+        }
+    }
+
+    public function proteksi_halaman()
+    {
+        if ($this->ci->session->userdata('username') == '') {
+            $this->ci->session->set_flashdata('error', 'Silahkan Login');
+            redirect('auth/login_user');
+        }
+    }
+
+    public function logout()
+    {
+        $this->ci->session->unset_userdata('username');
+        $this->ci->session->unset_userdata('nama_user');
+        $this->ci->session->unset_userdata('level_user');
+        $this->ci->session->set_flashdata('pesan', 'Logout Berhasil !!!');
+        redirect('auth/login_user');
+    }
+}
+
+/* End of file User_login.php */
---
+++ application/views/layout/v_wrapper_backend.php
@@ -0,0 +1,11 @@
+<?php
+
+//proteksi halaman
+$this->user_login->proteksi_halaman();
+
+//penggabungan berurut
+require_once('v_head.php');
+require_once('v_header_backend.php');
+require_once('v_nav_backend.php');
+require_once('v_content.php');
+require_once('v_footer_backend.php');
